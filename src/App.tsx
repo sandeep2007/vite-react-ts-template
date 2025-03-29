63fc2bfbf8f24ef58ce8b6033e993c0922c7f3dc
Sandeep Kumar <ki.sandeep11@gmail.com>
2025-03-29 12:44:55 +0530

    ci: test

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,6 @@ function App() {
   return (
     <>
       <div>
-        <div>Test</div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
